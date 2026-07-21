4c88f42ab95f15a3bc80eaa01c4b96fe15a93617
henrylonggg <henryl@udel.edu>
2026-07-21 11:23:42 -0400

    Load home navigation polish

--- lock-server.js
+++ lock-server.js
@@ -21,6 +21,7 @@ const replacement=`function sendApp(req,res){
   html=html.split('<script defer src="/interface-polish.js"></script>').join('');
   html=html.split('<script defer src="/final-fixes.js"></script>').join('');
   html=html.split('<script defer src="/lock-polish.js"></script>').join('');
+  html=html.split('<script defer src="/home-nav-polish.js"></script>').join('');
   html=html.replace('<option value="asshole">Asshole · online or AI</option>','');
   html=html.replace('<option value="asshole">Asshole · online or CPUs</option>','');
   html=html.replace('<option value="asshole" disabled="">Asshole - locked for rebuild</option>','');
@@ -31,7 +32,7 @@ const replacement=`function sendApp(req,res){
    .replace('🥇 Hard · 1.1 sec per count','🥇 Hard · 18 sec beer · 1.0 sec/count')
    .replace('Asshole - locked for rebuild','')
    .replace('Asshole is locked while it gets rebuilt.','');
-  const assets='<link rel="stylesheet" href="/lock-mode.css?v=controls-6"><link rel="stylesheet" href="/final-fixes.css?v=controls-6"><script defer src="/lock-mode.js?v=controls-6"></script><script defer src="/final-fixes.js?v=controls-6"></script><script defer src="/lock-polish.js?v=controls-6"></script>';
+  const assets='<link rel="stylesheet" href="/lock-mode.css?v=home-1"><link rel="stylesheet" href="/final-fixes.css?v=home-1"><script defer src="/lock-mode.js?v=home-1"></script><script defer src="/final-fixes.js?v=home-1"></script><script defer src="/lock-polish.js?v=home-1"></script><script defer src="/home-nav-polish.js?v=home-1"></script>';
   res.type('html').send(html.replace('</body>',assets+'</body>'));
  });
 }
