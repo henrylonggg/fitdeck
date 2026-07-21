5c9859ae2ede2bd018fddbb0cdfacf9153d799b3
henrylonggg <henryl@udel.edu>
2026-07-21 13:23:31 -0400

    Bump cache for Lethal Cross lock buttons

--- lock-server.js
+++ lock-server.js
@@ -40,7 +40,7 @@ const replacement=`function sendApp(req,res){
    .replace('<option value="asshole">Asshole · online or CPUs</option>','')
    .replace('<option value="asshole" disabled="">Asshole - locked for rebuild</option>','')
    .replace('<option value="asshole" disabled>Asshole - locked for rebuild</option>','');
-  const assets='<link rel="stylesheet" href="/lock-mode.css?v=navtap-1"><link rel="stylesheet" href="/final-fixes.css?v=navtap-1"><script defer src="/lock-mode.js?v=navtap-1"></script><script defer src="/final-fixes.js?v=navtap-1"></script><script defer src="/lock-polish.js?v=navtap-1"></script><script defer src="/home-nav-polish.js?v=navtap-1"></script><script defer src="/home-auth-guard.js?v=navtap-1"></script><script defer src="/game-flow-polish.js?v=navtap-1"></script><script defer src="/lock-button-pin.js?v=navtap-1"></script><script defer src="/nav-popup-final.js?v=navtap-1"></script>';
+  const assets='<link rel="stylesheet" href="/lock-mode.css?v=lcbuttons-1"><link rel="stylesheet" href="/final-fixes.css?v=lcbuttons-1"><script defer src="/lock-mode.js?v=lcbuttons-1"></script><script defer src="/final-fixes.js?v=lcbuttons-1"></script><script defer src="/lock-polish.js?v=lcbuttons-1"></script><script defer src="/home-nav-polish.js?v=lcbuttons-1"></script><script defer src="/home-auth-guard.js?v=lcbuttons-1"></script><script defer src="/game-flow-polish.js?v=lcbuttons-1"></script><script defer src="/lock-button-pin.js?v=lcbuttons-1"></script><script defer src="/nav-popup-final.js?v=lcbuttons-1"></script>';
   res.type('html').send(html.replace('</body>',assets+'</body>'));
  });
 }
